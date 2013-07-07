b846636d4f4baaace034fcfe3ef32e1f9c25686f
Simon Kim <spamhere@kaoma.net>
2013-07-07 11:55:05 +0900

    index.html as response

--- web.js
+++ web.js
@@ -1,12 +1,14 @@
-
-
-
 var express = require('express');
 
 var app = express.createServer(express.logger());
+var fs = require('fs');
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+
+  //response.send('Hello World 2!');
+  var buf;
+  buf = fs.readFileSync( 'index.html' );
+  response.send( buf.toString() );
 });
 
 var port = process.env.PORT || 5000;
